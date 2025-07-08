594e25bebf05ca0c5f77f4b4a45f5527a78df07b
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-07-08 16:33:55 +0200

    removed useless imports

--- src/main/java/team1/DAO/SellersDao.java
+++ src/main/java/team1/DAO/SellersDao.java
@@ -3,9 +3,8 @@ package team1.DAO;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.EntityTransaction;
-import org.hibernate.mapping.Selectable;
 import team1.entities.Sellers;
-import team1.entities.Ticket;
+
 
 public class SellersDao {
     private EntityManager entityManager;
