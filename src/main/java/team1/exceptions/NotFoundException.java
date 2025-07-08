6c91cc9775412c026f6e7692303831a2e497a2cb
gianlucamerlo97 <notoriousbig9700@gmail.com>
2025-07-08 16:54:47 +0200

    update error

--- src/main/java/team1/exceptions/NotFoundException.java
+++ src/main/java/team1/exceptions/NotFoundException.java
@@ -2,6 +2,6 @@ package team1.exceptions;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(long id) {
-        super("The element with id " + id + " hasn't been found yet or it doesn't exists!");
+        super("The element with id " + id + " hasn't been found yet or it doesn't exist!");
     }
 }
