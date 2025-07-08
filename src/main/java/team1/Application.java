2011954dcb1e81b4e7e8738383b749d31bac3ed9
gianlucamerlo97 <notoriousbig9700@gmail.com>
2025-07-08 16:33:01 +0200

    push application

--- src/main/java/team1/Application.java
+++ src/main/java/team1/Application.java
@@ -3,11 +3,19 @@ package team1;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import team1.DAO.LineDAO;
+import team1.DAO.VehicleLineJourneyDAO;
+import team1.entities.Line;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("trasporto");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em=emf.createEntityManager();
+        LineDAO ld= new LineDAO(em);
+        VehicleLineJourneyDAO vljd= new VehicleLineJourneyDAO(em);
+
+        
+
     }
 }
