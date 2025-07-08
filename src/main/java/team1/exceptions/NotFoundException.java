6d98b55d66e8dbebb5d36ce82419757311b759ce
gianlucamerlo97 <notoriousbig9700@gmail.com>
2025-07-08 16:38:03 +0200

    update

--- src/main/java/team1/exceptions/NotFoundException.java
+++ src/main/java/team1/exceptions/NotFoundException.java
@@ -2,6 +2,6 @@ package team1.exceptions;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(long id) {
-        super("L'evento con id " + id + " non è stato trovato o non esiste ancora!");
+        super("The element with id " + id + " hasn't been found yet or it doesn't exists!");
     }
 }
