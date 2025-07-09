ebe6c60f13c6cf0a5d784c483dcc62379ebacf54
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-07-09 17:25:57 +0200

    made method sell

--- src/main/java/team1/DAO/SellersDao.java
+++ src/main/java/team1/DAO/SellersDao.java
@@ -5,14 +5,19 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.EntityTransaction;
 import team1.entities.Sellers;
 import team1.entities.Ticket;
+import team1.entities.TravelCard;
 import team1.entities.User;
 import team1.entities.enums.State;
+import team1.entities.enums.TicketType;
 import team1.entities.sellersSons.TicketMachine;
 import team1.entities.ticketSons.SingleTicket;
+import team1.entities.ticketSons.SubscriptionTicket;
 import team1.exceptions.NotFoundException;
 import team1.exceptions.ReUsableException;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class SellersDao {
@@ -39,7 +44,7 @@ public class SellersDao {
         return sellerFromId;
     }
 
-    public Ticket sellTicket(Sellers seller, boolean n, User user) {
+    public Ticket sellTicket(Sellers seller, User user) {
         if (seller == null) {
             throw new EntityNotFoundException("this seller does not exist");
         }
@@ -49,35 +54,48 @@ public class SellersDao {
                 throw new ReUsableException("this machine is out of service");
             }
         }
-        if (n) {
-            SingleTicket newTicket = new SingleTicket(LocalDate.now(), seller);
-            System.out.println("New single ticket created");
-            return newTicket;
-        } else {
-            if (user.getTravelCard() == null) {
-                throw new Riusablexception("You can't create a subscription ticket without a travel card");
-            } else {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Select your type of subscription: 1: Weekly, 2: Monthly, 3: Yearly");
-                int typeSelection = Integer.parseInt(scanner.nextLine());
 
-                switch (typeSelection) {
-                    case 1:
-                        SubscriptionTicket weeklyTicket = new SubscriptionTicket(LocalDate.now(), seller, TicketType.WEEKLY, LocalDate.now().plusDays(7), user.getTravelCard());
-                        System.out.println("New weekly subscription ticket created");
-                        return weeklyTicket;
-                    case 2:
-                        SubscriptionTicket monthlyTicket = new SubscriptionTicket(LocalDate.now(), seller, TicketType.MONTHLY, LocalDate.now().plusMonths(1), user.getTravelCard());
-                        System.out.println("New monthly subscription ticket created");
-                        return monthlyTicket;
-                    case 3:
-                        SubscriptionTicket yearlyTicket = new SubscriptionTicket(LocalDate.now(), seller, TicketType.YEARLY, LocalDate.now().plusYears(1), user.getTravelCard());
-                        System.out.println("New yearly subscription ticket created");
-                        return yearlyTicket;
-                    default:
-                        throw new IllegalArgumentException("Invalid subscription type selected");
+        Scanner scanner  = new Scanner(System.in);
+        System.out.println("Which type of ticket do you need? 1 for single ticket, " +
+                "2 for weekly or monthly (IF YOU CHOOSE OPTIONS 2 YOU NEED A TRAVELCARD)");
+        int userChoice = Integer.parseInt(scanner.nextLine());
+        switch (userChoice){
+            case 1:
+                SingleTicket newTicket = new SingleTicket(LocalDate.now(), seller);
+                System.out.println("New single ticket created");
+                return newTicket;
+            case 2:
+                if(user.getAllTravelCards().isEmpty()){
+                    System.out.println("you cant buy this ticket");
+                }else{
+                    //questa e una lista io voglio vedere se nella lista c e una card valida
+                   List<TravelCard> cardStatus =  user.getAllTravelCards().stream()
+                            .filter(tc->tc.getExpiration_date().isAfter(LocalDate.now()))
+                                    .toList();
+
+                   //da vedere se la tessera e scaduta lo user deve crearne una nuova
+                    if(cardStatus.isEmpty()){
+                        throw new ReUsableException("you have to create another travel card");
+                    }
+
+                    System.out.println("2 for weekly 3 for monthly");
+                    int userChoice2 = Integer.parseInt(scanner.nextLine());
+                    switch (userChoice2){
+                        case 2:
+                            SubscriptionTicket newSubsciptionWeekly = new SubscriptionTicket(LocalDate.now(), seller,
+                                    TicketType.WEEKLY,cardStatus.getFirst());
+                            System.out.println("New  ticket created");
+                            return newSubsciptionWeekly;
+                        case 3:
+                            SubscriptionTicket newSubsciptionMonthly = new SubscriptionTicket(LocalDate.now(), seller,
+                                    TicketType.MONTHLY,cardStatus.getFirst());
+                            System.out.println("New  ticket created");
+                            return newSubsciptionMonthly;
+                    }
+
                 }
-            }
+            default:
+                throw new ReUsableException("value selected is not ok");
         }
     }
     
